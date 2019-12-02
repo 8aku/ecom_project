2c83b4cc4aabbc9c687369722446fd030451a758
8aku <churzed@gmail.com>
2019-12-02 13:01:27 -0600

    admin for all environments

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,10 @@
 AdminUser.destroy_all
 Product.destroy_all
 Dreamtype.destroy_all
+Customer.destroy_all
 Province.destroy_all
 
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 
 
 absurd = Dreamtype.create(
