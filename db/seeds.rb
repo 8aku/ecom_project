fb6d58249ce07ce193ee089418bdf5b74a81f3c3
8aku <churzed@gmail.com>
2019-12-02 12:09:28 -0600

    Seeding data now to avoid another deletion lol

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+AdminUser.destroy_all
+Product.destroy_all
+Dreamtype.destroy_all
+
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+
+   absurd = Dreamtype.create(
+      dreamtype: "AbsurdDream",
+      horror_rating: 7,
+      description: "You don't know who you are. You don't know what a dream is. Tis ABSURD.",
+      notes: "May increase suicidal behaviour.")
+   absurd.save!
+
+      comfort = Dreamtype.create(
+      dreamtype: "ComfortingDream",
+      horror_rating: 2,
+      description: "Yum, a warm piece of pie. You're fine with doing ANYTHING. Extra cozy.",
+      notes: "Could cause weight gain.")
+   comfort.save!
+
+
+   20.times do |i|
+   	absurd_product = Product.create(
+   		dreamtype: Dreamtype.where(dreamtype: "AbsurdDream").first,
+   		name: "AbsurdProduct_#{i}",
+   		stock: i,
+   		price_per_unit: 50,
+   		description: "Ab absurd-type product. Generally absurd.")
+   	absurd_product.save!
+   end
